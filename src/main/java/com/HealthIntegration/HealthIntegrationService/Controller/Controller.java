8581a86b5f5f07911cfe011230d8ca51b67af3cd
vijaykangude <vijaykangude.005@gmail.com>
2023-02-27 23:51:20 +0530

    endpoint updated to see changes reflects in deployed app or not

--- src/main/java/com/HealthIntegration/HealthIntegrationService/Controller/Controller.java
+++ src/main/java/com/HealthIntegration/HealthIntegrationService/Controller/Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-	@GetMapping('/')
+	@GetMapping("/")
 	public String Home(){
 		return "Welcome to Spring Boot App";
 	}
